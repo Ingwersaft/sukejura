f68adae921b8721b20198029454d9cf09714e2b1
Marcel Kesselring <Ingwersaft@users.noreply.github.com>
2018-10-30 11:59:42 +0100

    adapted gradle from kotlin 1.3 eap to 1.3 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.0-rc-190"
+    kotlin("jvm") version "1.3.0"
 }
 
 group = "io.kesselring.kron"
 version = "1.0.0-SNAPSHOT"
 
 repositories {
-    maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap") }
     mavenCentral()
     jcenter()
 }
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.0-RC1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.0")
 
     testCompile("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.1")
